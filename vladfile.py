4733913871eaf1368d4315bf4b5cf1daf5101435
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2020-02-27 09:33:27 +0000

    black

--- vladfile.py
+++ vladfile.py
@@ -12,7 +12,11 @@ class Validator(Vlad):
         "doi": [RegexValidator(r"[\w\-./]+", full=True, empty_ok=True)],
         "funders": [RegexValidator(r"(\d+;?)+", full=True, empty_ok=True)],
         "homepage_url": [RegexValidator(r"https?://.+", full=True, empty_ok=True)],
-        "licence": [SetValidator(["MIT", "GPL-2.0", "Artistic-2.0", "BSD-3-Clause"], empty_ok=True)],
+        "licence": [
+            SetValidator(
+                ["MIT", "GPL-2.0", "Artistic-2.0", "BSD-3-Clause"], empty_ok=True
+            )
+        ],
         "organisations": [SetValidator(["grid.457348.9"], empty_ok=True)],
         "rsotm": [RegexValidator(r"\d{4}-\d{2}", full=True, empty_ok=True)],
         "url": [UniqueValidator()],
